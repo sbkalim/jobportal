5dac5bbe9427c2a75d27939fef518dbf6f75cb5b
Shaif Bin Kalim <sbkalim@ymail.com>
2023-08-11 12:39:23 +0600

    Added website app and job app

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -31,20 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
-    "dashboard",
-    "company",
-    "resume",
-    "users",
+    'company',
+    'dashboard',
+    'job',
+    'resume',
+    'users',
+    'website',
 
-    "widget_tweaks",
-
+    'widget_tweaks',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -124,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- jobportal/urls.py
+++ jobportal/urls.py
@@ -20,9 +20,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('website.urls')),
     path("admin/", admin.site.urls),
     path('accounts/', include('users.urls')),
     path('company/', include('company.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('resume/', include('resume.urls')),
+    path('job/', include('job.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
